09c1117d7b4dc780fa79d7241cda3c37b2b0aeab
ElinRin <elin.rinnel@gmail.com>
2017-12-07 22:03:58 +0300

    Updates

--- src/components/TimelineDelimiter/TimelineDelimiter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './TimelineDelimiter.css';
-
-export default () => (
-  <div className='TimelineDelimiter'>Покупки</div>
-);
--- src/services/database.js
+++
@@ -1,5 +0,0 @@
-import firebaseInit from './firebase.js';
-
-import 'firebase/database'
-
-export default firebaseInit().database();
